430b10da86e778f6b4120a96bc6e22059b4d8442
esjee <ewfawefw3@gmail.com>
2017-03-08 21:02:07 +0100

    fix indentation

--- spec/hackerone/report_spec.rb
+++ spec/hackerone/report_spec.rb
@@ -5,8 +5,8 @@ describe Hackerone::Report do
     it 'fetches a report by id' do
       report = Hackerone::Report.find_by id: 211770
 
-       expect(report).to be_present
-       expect(report.id).to be_present
+      expect(report).to be_present
+      expect(report.id).to be_present
       expect(report.title).to eq 'kerk'
       expect(report.vulnerability_information).to be_present
     end
